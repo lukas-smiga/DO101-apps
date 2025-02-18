0b030df425b8754a7798793de7aa07329611276d
lukas-smiga <lukas.smiga@elostech.cz>
2025-02-18 08:53:07 -0500

    config change

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,5 +1,11 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
+
+
+const USER = process.env.USER
+const PASS = process.env.PASS
+const DB = process.env.DB
+
+const DB_CONFIG = 'postgresql://' + USER + ':' + PASS + '@contactsdb.svc.cluster.local:5432/' + DB;
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
