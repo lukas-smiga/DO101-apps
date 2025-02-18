31007e35f74c45b51367bfdff3ef24009b7d3f01
lukas-smiga <lukas.smiga@elostech.cz>
2025-02-18 08:57:36 -0500

    fixed url

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -5,7 +5,7 @@ const USER = process.env.USER
 const PASS = process.env.PASS
 const DB = process.env.DB
 
-const DB_CONFIG = 'postgresql://' + USER + ':' + PASS + '@contactsdb.svc.cluster.local:5432/' + DB;
+const DB_CONFIG = 'postgresql://' + USER + ':' + PASS + '@contactsdb.dxkdcr-contacts.svc.cluster.local:5432/' + DB;
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
